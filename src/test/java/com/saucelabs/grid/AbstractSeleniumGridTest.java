34df3a4ca0a7fa0d0919eb072e875f9afaf061b0
Alasdair Macmillan <aimmac23@gmail.com>
2014-01-23 11:52:00 +0000

    Finish off writing a test which can build its own selneium grid, and verify that the nodes are being used

--- src/test/java/com/saucelabs/grid/AbstractSeleniumGridTest.java
+++ src/test/java/com/saucelabs/grid/AbstractSeleniumGridTest.java
@@ -1,20 +1,49 @@
 package com.saucelabs.grid;
 
+import static junit.framework.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.message.BasicHttpRequest;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.grid.common.RegistrationRequest;
+import org.openqa.grid.common.exception.GridException;
 import org.openqa.grid.web.Hub;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.internal.HttpClientFactory;
 
 import com.saucelabs.grid.utils.TestHelper;
 
+/**
+ * An abstract test skeleton that helps start up a fresh selenium grid for each test.
+ * 
+ * Currently not thread-safe.
+ * 
+ * @author Alasdair Macmillan
+ *
+ */
 public abstract class AbstractSeleniumGridTest {
 	
 	public static final int HUB_PORT = 7777;
 	protected Hub hub; 
+    protected HttpClientFactory httpClientFactory;
+
 	
 	@Before
 	public void setup() throws Exception {
 		// we'll always need a hub
 		hub = TestHelper.getHub(HUB_PORT);
+	    this.httpClientFactory = new HttpClientFactory();
+
 	}
 	
 	@After
@@ -22,5 +51,74 @@ public abstract class AbstractSeleniumGridTest {
 		hub.stop();
 	}
 	
+	protected JSONObject getJSONFromURL(HttpHost host, String uri) {
+		HttpClient client = httpClientFactory.getHttpClient();
+		
+		BasicHttpRequest request = new BasicHttpRequest("GET", uri);
+		try {
+			HttpResponse response = client.execute(host, request);
+			if (response.getStatusLine().getStatusCode() != 200) {
+				throw new GridException("Hub is down or not responding.");
+			}
+			
+			return extractObject(response);
+			
+		} catch (Exception e) {
+			throw new GridException("Hub is down or not responding: "
+					+ e.getMessage());
+		}
+	}
+	
+	/**
+	 * Copy-pasted from SelfRegisteringRemote, because it was defined as private.
+	 * @param resp
+	 * @return
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	  private static JSONObject extractObject(HttpResponse resp) throws IOException, JSONException {
+		    BufferedReader rd = new BufferedReader(new InputStreamReader(resp.getEntity().getContent()));
+		    StringBuilder s = new StringBuilder();
+		    String line;
+		    while ((line = rd.readLine()) != null) {
+		      s.append(line);
+		    }
+		    rd.close();
+		    return new JSONObject(s.toString());
+	  }
+	  
+	protected void assertNodeHandledSession(RemoteWebDriver driver, int port) throws JSONException {
+		String sessionKey = driver.getSessionId().toString();
+
+		JSONObject json = getJSONFromURL(new HttpHost("127.0.0.1", port),
+				"/wd/hub/sessions");
+
+		JSONArray sessions = json.getJSONArray("value");
+		assertEquals(sessions.length(), 1);
+
+		JSONObject sessionInfo = sessions.getJSONObject(0);
+		assertEquals(sessionKey, sessionInfo.getString("id"));
+	}
+	
+    
+    protected RegistrationRequest buildRegistrationRequest(int nodePort) {
+        RegistrationRequest req = RegistrationRequest.build("-role", "node" , "-host", "localhost");
+
+        req.getConfiguration().put(RegistrationRequest.PORT, nodePort);
+
+        req.getConfiguration().put(RegistrationRequest.HUB_HOST, hub.getHost());
+        req.getConfiguration().put(RegistrationRequest.HUB_PORT, hub.getPort());
+        
+        String url =
+                "http://" + req.getConfiguration().get(RegistrationRequest.HOST) + ":"
+                    + req.getConfiguration().get(RegistrationRequest.PORT);
+            req.getConfiguration().put(RegistrationRequest.REMOTE_HOST, url);
+        
+        // we don't want silly defaults
+        req.getCapabilities().clear();
+        
+        return req;
+
+    }
 
 }
--- src/test/java/com/saucelabs/grid/HtmlUnitTest.java
+++ src/test/java/com/saucelabs/grid/HtmlUnitTest.java
@@ -4,6 +4,7 @@ import static junit.framework.Assert.assertEquals;
 
 import java.net.URL;
 
+import org.json.JSONException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,10 +18,11 @@ public class HtmlUnitTest extends AbstractSeleniumGridTest {
 
     private RemoteWebDriver driver;
 	private SelfRegisteringRemote node;
+	private int nodePort;
 
     @Before
     public void setUp() throws Exception {
-    	int nodePort = PortProber.findFreePort();
+    	nodePort = PortProber.findFreePort();
     	RegistrationRequest request = buildRegistrationRequest(nodePort);
     	request.addDesiredCapability(DesiredCapabilities.htmlUnit());
     	
@@ -37,51 +39,29 @@ public class HtmlUnitTest extends AbstractSeleniumGridTest {
     }
     
     @Test
-    public void amazonTest() {
+    public void amazonTest() throws JSONException {
     	driver.get("http://www.amazon.com/");
     	
         assertEquals(
                 "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more",
                 driver.getTitle());
         
-        String sessionKey = driver.getSessionId().toString();
-        
+        assertNodeHandledSession(driver, nodePort);
     }
     
     
     @Override
 	@After
     public void tearDown() throws Exception {
+
+    	if(driver != null) {
+    		driver.quit();	
+    	}
     	
     	if(node != null) {
     		node.stopRemoteServer();
     	}
     	
-    	if(driver != null) {
-    		driver.quit();	
-    	}
     	super.tearDown();
-    }
-    
-    private RegistrationRequest buildRegistrationRequest(int nodePort) {
-        RegistrationRequest req = RegistrationRequest.build("-role", "node" , "-host", "localhost");
-
-        req.getConfiguration().put(RegistrationRequest.PORT, nodePort);
-
-        req.getConfiguration().put(RegistrationRequest.HUB_HOST, hub.getHost());
-        req.getConfiguration().put(RegistrationRequest.HUB_PORT, hub.getPort());
-        
-        String url =
-                "http://" + req.getConfiguration().get(RegistrationRequest.HOST) + ":"
-                    + req.getConfiguration().get(RegistrationRequest.PORT);
-            req.getConfiguration().put(RegistrationRequest.REMOTE_HOST, url);
-
-        
-        // we don't want silly defaults
-        req.getCapabilities().clear();
-
-        
-        return req;
-
     }
 }
